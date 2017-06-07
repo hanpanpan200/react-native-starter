1e278fae3eefeafef698cf2f2b2f433df84f64b2
Panpan Han <hanpanpan200@gmail.com>
2017-06-07 11:18:07 +0800

    feat: integrate navigation & setup jest

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
 import React from 'react'
-import {
-  StyleSheet,
-  View,
-  Text,
-} from 'react-native'
 import { Provider } from 'react-redux'
 import store from './redux/store'
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-})
+import AppNavigator from './AppNavigator'
 
 const App = () => (
   <Provider store={store}>
-    <View style={styles.container}>
-      <Text style={styles.welcome}>
-        Welcome to React Native!
-      </Text>
-      <Text style={styles.instructions}>
-        To get started, edit index.ios.js
-      </Text>
-      <Text style={styles.instructions}>
-        Press Cmd+R to reload,{'\n'}
-        Cmd+D or shake for dev menu
-      </Text>
-    </View>
+    <AppNavigator />
   </Provider>
 )
 

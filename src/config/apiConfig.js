a28c6cd890e2f807e39e6ccd353fbf2696bfd301
Panpan Han <hanpanpan200@gmail.com>
2017-06-07 17:22:32 +0800

    feat: remove config file & add api config sample

--- src/config/apiConfig.js
+++
@@ -1,7 +0,0 @@
-const leanCloud = {
-  API_HOST: 'https://leancloud.cn:443/1.1',
-  API_ID: 'IFKQniqghtnQCD1HSkYhfd8r-gzGzoHsz',
-  API_KEY: 'EUvdEq5RslkTDjXkPxsAX9PS',
-}
-
-export default leanCloud

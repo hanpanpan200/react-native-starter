af249d09e3d382578cd6808d74b7d174a3cf728a
Panpan Han <hanpanpan200@gmail.com>
2017-06-20 16:56:44 +0800

    feat: add action

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { View, Text } from 'react-native'
 
 class Login extends Component {
+  static propTypes = {
+    login: PropTypes.func.isRequired,
+  }
+
+  login = () => {
+    this.props.login()
+  }
+
   render() {
     return (
       <View>
-        <Text>Login</Text>
+        <Text onPress={this.login}>Login</Text>
       </View>
     )
   }
